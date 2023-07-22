bf2b98c539debbb34117da9e35be7e0890358e35
Anas Ismail <53524432+anasis22@users.noreply.github.com>
2023-07-22 14:21:47 +0530

    Updated

--- first-repo/src/App.js
+++ first-repo/src/App.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 const App = () => {
 
-  let a = 20;
   return (
     <div>
-      <h2>Hello World{a}</h2>
+      <h2>Hello World</h2>
       <h3>Here another hello world</h3>
       <h3>another change</h3>
       <h3>lolololol</h3>
