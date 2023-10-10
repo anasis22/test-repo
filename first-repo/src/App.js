2f71a8d9ba397b25723f6a4aa2904613245210e4
anas ismail <msanasismail@gmail.com>
2023-10-10 17:11:58 +0530

    app modified

--- first-repo/src/App.js
+++ first-repo/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <div>
       <h2>Hello World{a}</h2>
       <h3>Here another hello world</h3>
-      <h3>mine</h3>
+      <h3>mine1234</h3>
       <h3>how are you?</h3>
       <h3>anas ismail</h3>
     </div>
